309c27614df1d9d708e33e7d1be5f65b4ddad993
tn <noack@esa.tu-darmstadt.de>
2024-04-17 15:20:38 +0200

    Move definitions of cpu specific macros from Clang::RenderTargetOptions to ColossusTargetInfo

--- clang/lib/Basic/Targets/Colossus.h
+++ clang/lib/Basic/Targets/Colossus.h
@@ -37,155 +37,203 @@
 #define LLVM_CLANG_LIB_BASIC_TARGETS_COLOSSUS_H
 
 #include "clang/Basic/Diagnostic.h"
-#include "clang/Basic/TargetInfo.h"
 #include "clang/Basic/TargetBuiltins.h"
+#include "clang/Basic/TargetInfo.h"
 
 namespace clang {
-namespace targets {
-}
+namespace targets {}
 class LLVM_LIBRARY_VISIBILITY ColossusTargetInfo : public TargetInfo {
-    static const Builtin::Info BuiltinInfo[];
-  public:
-    static const char * const GCCRegNames[];
-    static const TargetInfo::GCCRegAlias GCCRegAliases[];
+  static const Builtin::Info BuiltinInfo[];
 
-    ColossusTargetInfo(const llvm::Triple &Triple, const TargetOptions &)
-        : TargetInfo(Triple) {
-      BigEndian = false;
-      NoAsmVariants = true;
-      LongAlign = 32;
-      LongWidth = 32;
-      LongLongAlign = 64;
-      LongLongWidth = 64;
-      SuitableAlign = 32;
-      DoubleAlign = LongDoubleAlign = 32;
-      MaxVectorAlign = 64;
-      WIntType = UnsignedInt;
-      UseZeroLengthBitfieldAlignment = true;
-      MaxAtomicPromoteWidth = MaxAtomicInlineWidth = 32;
-      PtrDiffType = IntPtrType = SignedInt;
-      SizeType = UnsignedInt;
-      HasStrictFP = true;
-      resetDataLayout("e-m:e-p:32:32-i1:8:32-i8:8:32-i16:16:32-i64:32-i128:64"
-                      "-f64:32-f128:64-v128:64-a:0:32-n32");
-    }
-    void getTargetDefines(const LangOptions &Opts,
-                          MacroBuilder &Builder) const override {
-    }
+public:
+  static const char *const GCCRegNames[];
+  static const TargetInfo::GCCRegAlias GCCRegAliases[];
 
-    ArrayRef<Builtin::Info> getTargetBuiltins() const override {
-      return llvm::makeArrayRef(BuiltinInfo,
-               clang::Colossus::LastTSBuiltin-Builtin::FirstTSBuiltin);
-    }
+  std::string CPU;
+  bool Supervisor = false;
+  bool Worker = false;
 
-    BuiltinVaListKind getBuiltinVaListKind() const override {
-      return TargetInfo::VoidPtrBuiltinVaList;
-    }
-
-    std::string_view getClobbers() const override {
-      return "";
-    }
-
-    bool isValidCPUName(StringRef Name) const override {
-      return Name.startswith("ipu");
-    }
-
-    bool setCPU(const std::string &Name) override {
-      return isValidCPUName(Name);
-    }
-
-    ArrayRef<const char *> getGCCRegNames() const override;
-
-    ArrayRef<TargetInfo::GCCRegAlias> getGCCRegAliases() const override;
-
-    bool validateAsmConstraint(const char *&Name,
-                               TargetInfo::ConstraintInfo &Info) const override {
-      return false;
+  ColossusTargetInfo(const llvm::Triple &Triple, const TargetOptions &Opts)
+      : TargetInfo(Triple) {
+    BigEndian = false;
+    NoAsmVariants = true;
+    LongAlign = 32;
+    LongWidth = 32;
+    LongLongAlign = 64;
+    LongLongWidth = 64;
+    SuitableAlign = 32;
+    DoubleAlign = LongDoubleAlign = 32;
+    MaxVectorAlign = 64;
+    WIntType = UnsignedInt;
+    UseZeroLengthBitfieldAlignment = true;
+    MaxAtomicPromoteWidth = MaxAtomicInlineWidth = 32;
+    PtrDiffType = IntPtrType = SignedInt;
+    SizeType = UnsignedInt;
+    HasStrictFP = true;
+    CPU = Opts.CPU;
+    resetDataLayout("e-m:e-p:32:32-i1:8:32-i8:8:32-i16:16:32-i64:32-i128:64"
+                    "-f64:32-f128:64-v128:64-a:0:32-n32");
+  }
+  void getTargetDefines(const LangOptions &Opts,
+                        MacroBuilder &Builder) const override {
+    Builder.defineMacro("__IPU__");
+    if (CPU == "ipu1") {
+      Builder.defineMacro("__IPU_ARCH_VERSION__", "1");
+      Builder.defineMacro("__IPU_REPEAT_COUNT_SIZE__", "12");
+    } else if (CPU == "ipu2") {
+      Builder.defineMacro("__IPU_ARCH_VERSION__", "2");
+      Builder.defineMacro("__IPU_REPEAT_COUNT_SIZE__", "16");
+#ifdef IPU21
+    } else if (CPU == "ipu21") {
+      Builder.defineMacro("__IPU_ARCH_VERSION__", "21");
+      Builder.defineMacro("__IPU_REPEAT_COUNT_SIZE__", "16");
+#endif
+    } else {
+      llvm::errs() << "Unsupported CPU " << CPU << "\n";
     }
 
-    bool initFeatureMap(llvm::StringMap<bool> &Features,
-                              DiagnosticsEngine &Diags, StringRef CPU,
-                              const std::vector<std::string> &FeaturesVec) const override;
-
-    bool hasBitIntType() const override { return true; }
-  };
-
-  const char *const ColossusTargetInfo::GCCRegNames[] = {
-    "$m0" , "$m1" , "$m2" , "$m3" , "$m4" , "$m5" , "$m6" , "$m7" ,
-    "$m8" , "$m9" , "$m10", "$m11", "$m12", "$m13", "$m14", "$m15",
-    "$a0" , "$a1" , "$a2" , "$a3" , "$a4" , "$a5" , "$a6" , "$a7" ,
-    "$a8" , "$a9" , "$a10", "$a11", "$a12", "$a13", "$a14", "$a15",
-    "$a0:1", "$a2:3", "$a4:5", "$a6:7", 
-  };
-
-  const TargetInfo::GCCRegAlias ColossusTargetInfo::GCCRegAliases[] = {
-      {{"$bp"}, "$m8"},
-      {{"$fp"}, "$m9"},
-      {{"$lr"}, "$m10"},
-      {{"$sp"}, "$m11"},
-      {{"$mworker_base"}, "$m12"},
-      {{"$mvertex_base"}, "$m13"},
-      {{"$azero"}, "$a15"},
-      {{"$mzero"}, "$m15"},
-  };
-
-  ArrayRef<const char*> ColossusTargetInfo::getGCCRegNames() const {
-    return llvm::makeArrayRef(GCCRegNames);
+    if (Supervisor) {
+      Builder.defineMacro("__SUPERVISOR__");
+    }
   }
 
-  ArrayRef<TargetInfo::GCCRegAlias> ColossusTargetInfo::
-  getGCCRegAliases() const {
-    return llvm::makeArrayRef(GCCRegAliases);
+  ArrayRef<Builtin::Info> getTargetBuiltins() const override {
+    return llvm::ArrayRef(BuiltinInfo, clang::Colossus::LastTSBuiltin -
+                                           Builtin::FirstTSBuiltin);
   }
 
-  // FIXME: Move this to a new Colossus.cpp file later.
-  bool ColossusTargetInfo::initFeatureMap(llvm::StringMap<bool> &Features,
-                              DiagnosticsEngine &Diags, StringRef CPU,
-                              const std::vector<std::string> &FeaturesVec) const {
-    // This keeps track of "-msupervisor" which resolves to "-worker".
-    // We override this if any explicit target attribute is set in the
-    // function (this is to be changed later).
-    bool hasImplicitSupervisorTarget = false;
+  BuiltinVaListKind getBuiltinVaListKind() const override {
+    return TargetInfo::VoidPtrBuiltinVaList;
+  }
 
-    StringRef TargetFeat;
-    std::vector<std::string> UpdatedFeaturesVec;
-    for (const std::string &F : FeaturesVec) {
-      if (F == "-worker") {
-        hasImplicitSupervisorTarget = true;
-      } else if (F == "+worker" || F == "+supervisor" || F == "+both") {
-        // Sets the explicit target feature found in function attributes.
-        // This must not be set twice as they are mutually exclusive.
-        if (!TargetFeat.empty()) {
-          Diags.Report(diag::err_opt_not_valid_with_opt) << TargetFeat << F;
-          return false;
-        }
-        TargetFeat = F;
-      } else {
-        UpdatedFeaturesVec.push_back(F);
+  std::string_view getClobbers() const override { return ""; }
+
+  bool isValidCPUName(StringRef Name) const override {
+    return Name == "ipu1" || Name == "ipu2" || Name == "ipu21";
+  }
+
+  bool setCPU(const std::string &Name) override {
+    if (isValidCPUName(Name)) {
+      CPU = Name;
+      return true;
+    }
+    return false;
+  }
+
+  ArrayRef<const char *> getGCCRegNames() const override;
+
+  ArrayRef<TargetInfo::GCCRegAlias> getGCCRegAliases() const override;
+
+  bool validateAsmConstraint(const char *&Name,
+                             TargetInfo::ConstraintInfo &Info) const override {
+    return false;
+  }
+
+  bool
+  initFeatureMap(llvm::StringMap<bool> &Features, DiagnosticsEngine &Diags,
+                 StringRef CPU,
+                 const std::vector<std::string> &FeaturesVec) const override;
+
+  bool hasBitIntType() const override { return true; }
+
+  bool handleTargetFeatures(std::vector<std::string> &Features,
+                            DiagnosticsEngine &Diags) override;
+};
+
+const char *const ColossusTargetInfo::GCCRegNames[] = {
+    "$m0",  "$m1",  "$m2",  "$m3",  "$m4",  "$m5",   "$m6",   "$m7",   "$m8",
+    "$m9",  "$m10", "$m11", "$m12", "$m13", "$m14",  "$m15",  "$a0",   "$a1",
+    "$a2",  "$a3",  "$a4",  "$a5",  "$a6",  "$a7",   "$a8",   "$a9",   "$a10",
+    "$a11", "$a12", "$a13", "$a14", "$a15", "$a0:1", "$a2:3", "$a4:5", "$a6:7",
+};
+
+const TargetInfo::GCCRegAlias ColossusTargetInfo::GCCRegAliases[] = {
+    {{"$bp"}, "$m8"},
+    {{"$fp"}, "$m9"},
+    {{"$lr"}, "$m10"},
+    {{"$sp"}, "$m11"},
+    {{"$mworker_base"}, "$m12"},
+    {{"$mvertex_base"}, "$m13"},
+    {{"$azero"}, "$a15"},
+    {{"$mzero"}, "$m15"},
+};
+
+ArrayRef<const char *> ColossusTargetInfo::getGCCRegNames() const {
+  return llvm::ArrayRef(GCCRegNames);
+}
+
+ArrayRef<TargetInfo::GCCRegAlias> ColossusTargetInfo::getGCCRegAliases() const {
+  return llvm::ArrayRef(GCCRegAliases);
+}
+
+// FIXME: Move this to a new Colossus.cpp file later.
+bool ColossusTargetInfo::initFeatureMap(
+    llvm::StringMap<bool> &Features, DiagnosticsEngine &Diags, StringRef CPU,
+    const std::vector<std::string> &FeaturesVec) const {
+  // This keeps track of "-msupervisor" which resolves to "-worker".
+  // We override this if any explicit target attribute is set in the
+  // function (this is to be changed later).
+  bool hasImplicitSupervisorTarget = false;
+
+  StringRef TargetFeat;
+  std::vector<std::string> UpdatedFeaturesVec;
+  for (const std::string &F : FeaturesVec) {
+    if (F == "-worker") {
+      hasImplicitSupervisorTarget = true;
+    } else if (F == "+worker" || F == "+supervisor" || F == "+both") {
+      // Sets the explicit target feature found in function attributes.
+      // This must not be set twice as they are mutually exclusive.
+      if (!TargetFeat.empty()) {
+        Diags.Report(diag::err_opt_not_valid_with_opt) << TargetFeat << F;
+        return false;
       }
+      TargetFeat = F;
+    } else {
+      UpdatedFeaturesVec.push_back(F);
     }
-
-    // Either have the explicit target attribute or the implicit from
-    // "-msupervisor" option. If neither, we default to "+worker".
-    if (!TargetFeat.empty())
-      UpdatedFeaturesVec.push_back(TargetFeat.str());
-    else if (hasImplicitSupervisorTarget)
-      UpdatedFeaturesVec.push_back("+supervisor");
-    else
-      UpdatedFeaturesVec.push_back("+worker");
-  
-    return TargetInfo::initFeatureMap(Features, Diags, CPU, UpdatedFeaturesVec);
   }
 
-  const Builtin::Info ColossusTargetInfo::BuiltinInfo[] = {
-#define BUILTIN(ID, TYPE, ATTRS) \
-  { #ID, TYPE, ATTRS, nullptr, HeaderDesc::NO_HEADER, ALL_LANGUAGES },
-#define LIBBUILTIN(ID, TYPE, ATTRS, HEADER) \
-  { #ID, TYPE, ATTRS, HEADER, HeaderDesc::NO_HEADER, ALL_LANGUAGES },
+  // Either have the explicit target attribute or the implicit from
+  // "-msupervisor" option. If neither, we default to "+worker".
+  if (!TargetFeat.empty())
+    UpdatedFeaturesVec.push_back(TargetFeat.str());
+  else if (hasImplicitSupervisorTarget)
+    UpdatedFeaturesVec.push_back("+supervisor");
+  else
+    UpdatedFeaturesVec.push_back("+worker");
+
+  return TargetInfo::initFeatureMap(Features, Diags, CPU, UpdatedFeaturesVec);
+}
+
+bool ColossusTargetInfo::handleTargetFeatures(
+    std::vector<std::string> &Features, DiagnosticsEngine &Diags) {
+
+  for (const auto &Feature : Features) {
+    if (Feature == "+worker") {
+      HalfArgsAndReturns = true;
+      HasLegalHalfType = true;
+      HasFloat16 = true;
+      Worker = true;
+      llvm::outs() << "Compiling for worker\n";
+    }
+    if (Feature == "+supervisor") {
+      llvm::outs() << "Compiling for supervisor\n";
+      Supervisor = true;
+    }
+  }
+
+  return true;
+}
+
+const Builtin::Info ColossusTargetInfo::BuiltinInfo[] = {
+#define BUILTIN(ID, TYPE, ATTRS)                                               \
+  {#ID, TYPE, ATTRS, nullptr, HeaderDesc::NO_HEADER, ALL_LANGUAGES},
+#define LIBBUILTIN(ID, TYPE, ATTRS, HEADER)                                    \
+  {#ID, TYPE, ATTRS, HEADER, HeaderDesc::NO_HEADER, ALL_LANGUAGES},
 #define TARGET_BUILTIN(ID, TYPE, ATTRS, FEATURE)                               \
   {#ID, TYPE, ATTRS, FEATURE, HeaderDesc::NO_HEADER, ALL_LANGUAGES},
 #include "clang/Basic/BuiltinsColossus.def"
-  };
-}
+};
+} // namespace clang
 
 #endif // LLVM_CLANG_LIB_BASIC_TARGETS_COLOSSUS_H
